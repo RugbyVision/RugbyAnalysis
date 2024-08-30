a93045d1fb60ddfaac82e4942bcaee7917ff89ab
CycloneFox19 <satoru21131@gmail.com>
2024-08-30 16:54:44 +0900

    Update StoringArray.py
    On the contrary to its name, StoringArray.py contains the function which imports array and print the contents.

--- PlayerDetection/Source/StoringArray.py
+++ PlayerDetection/Source/StoringArray.py
@@ -1,8 +1,10 @@
 import h5py
 import numpy as np
 
-path = "PlayerDetection/Resource/Arrays/RugbyGame_0.h5"
+path = "PlayerDetection/Resource/Arrays/RugbyGame_0.h5" # change as you like
 
+# read hdf5 file
 f = h5py.File(path, 'r')
+# obtain the arrays in ndarray form
 for i in range(345):
     print(np.array(f["Frame_" + str(i) + "_People"]))
